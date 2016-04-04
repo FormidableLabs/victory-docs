ad66d60af05cc9c9cd38350a9fcdf3100e386ae0
Paula Lavalle <lavalle.paula@gmail.com>
2016-04-04 13:25:12 -0700

    WIP remove unused styles
    TODO: Use an animating example on homepage

--- src/screens/home/index.jsx
+++ src/screens/home/index.jsx
@@ -6,6 +6,9 @@ import { Link } from "react-router";
 import Ecology from "ecology";
 import { VictoryChart, VictoryLine, VictoryPie } from "victory";
 
+// Settings
+import { VictorySettings } from "formidable-landers";
+
 // Child Components
 import Hero from "./components/hero";
 
@@ -23,6 +26,19 @@ class Home extends React.Component {
           margin: 0,
           padding: "60px 1rem"
         }
+      },
+      demoHeading: {
+        marginTop: 0,
+        color: VictorySettings.red
+      },
+      demo: {
+        marginTop: `${VictorySettings.gutter}px`,
+        padding: `${VictorySettings.gutter*3}px ${VictorySettings.gutter*2}px`,
+        backgroundColor: VictorySettings.palestSand
+      },
+      ecology: {
+        marginLeft: `-${VictorySettings.gutter*2}px`,
+        marginRight: `-${VictorySettings.gutter*2}px`
       }
     };
   }
@@ -33,66 +49,65 @@ class Home extends React.Component {
     return (
       <section style={styles.container}>
         <Hero />
-        <div className="Row">
-          <div className="u-textCenter">
-            <code className="Installer">npm install victory</code>
+
+        <div style={styles.demo}>
+          <h2 style={styles.demoHeading}>Try it</h2>
+          <p style={{maxWidth: "38em", marginLeft: "auto", marginRight: "auto"}}>
+            <span className="Smallcaps">Editor’s note.</span>
+            All of the following code examples in Victory Documentation are powered by our very own <a href="https://github.com/FormidableLabs/component-playground">
+              Component Playground
+            </a>; it renders editable source code and automatically updates the preview of the rendered component.
+          </p>
+          <div style={styles.ecology}>
+            <Ecology
+              overview={require("!!raw!./examples.md")}
+              scope={{React, ReactDOM, VictoryChart, VictoryLine, VictoryPie}}
+              playgroundtheme="elegant"
+            />
           </div>
         </div>
+
+        <h2>Benefits</h2>
+
+        <h3>Friendly</h3>
+        <p>
+          <span className="Smallcaps">1.</span>
+          The modular, componentized nature of React has allowed us to write fully-contained, reusable data visualization elements that are responsible for their own styles and behaviors.
+        </p>
+
+        <h3>Flexible</h3>
+        <p>
+          <span className="Smallcaps">2.</span>
+          The use of sensible default props makes getting started very easy, without sacrificing flexibility. Victory also leverages React lifecycle methods and DOM diffing to create a lightweight animation wrapper.
+        </p>
+
+        <h3>Composable</h3>
+        <p>
+          <span className="Smallcaps">3.</span>
+          When combined, these features result in a set of components that are easy to use, and compose into more complicated visualizations.
+        </p>
 
         <div>
-          <Ecology
-            overview={require("!!raw!./examples.md")}
-            scope={{React, ReactDOM, VictoryChart, VictoryLine, VictoryPie}}
-            playgroundtheme="base16-dark"
-          />
+          <Link className="Button Button--spotlight" to="docs">Get Started</Link>
         </div>
 
-        <div className="Row">
-          <h2 className="u-textCenter">Friendly</h2>
-          <p className="Copy">
-            The modular, componentized nature of React has allowed us to write fully-contained, reusable data visualization elements that are responsible for their own styles and behaviors.
-          </p>
-        </div>
-
-        <div className="Row">
-          <h2 className="u-textCenter">Flexible</h2>
-          <p className="Copy">
-            The use of sensible default props makes getting started very easy, without sacrificing flexibility. Victory also leverages React lifecycle methods and DOM diffing to create a lightweight animation wrapper.
-          </p>
-        </div>
-
-        <div className="Row">
-          <h2 className="u-textCenter">Composable</h2>
-          <p className="Copy">
-            When combined, these features result in a set of components that are easy to use, and compose into more complicated visualizations.
-          </p>
-        </div>
-
-        <div className="Row">
-          <div className="Copy u-textCenter">
-            <Link className="Button Button--spotlight" to="docs">Get Started</Link>
-          </div>
-        </div>
-
-        <div className="Row">
-          <h3 className="u-textCenter">Quick links:</h3>
-          <p className="Copy">
-            GitHub: <a href="https://github.com/FormidableLabs/victory">
-              https://github.com/FormidableLabs/victory
-            </a>
-          </p>
-          <p className="Copy">
-            Gitter chatroom: <a href="https://gitter.im/FormidableLabs/victory">
-              https://gitter.im/FormidableLabs/victory
-            </a>
-          </p>
-          <p className="Copy">
-            Roadmap: <a href="https://github.com/FormidableLabs/victory/blob/master/ROADMAP.md">
-              ROADMAP.md
-            </a>
-          </p>
-          <p className="Copy">Component docs:</p>
-        </div>
+        <h2>Quick links:</h2>
+        <p>
+          GitHub: <a href="https://github.com/FormidableLabs/victory">
+            https://github.com/FormidableLabs/victory
+          </a>
+        </p>
+        <p>
+          Gitter chatroom: <a href="https://gitter.im/FormidableLabs/victory">
+            https://gitter.im/FormidableLabs/victory
+          </a>
+        </p>
+        <p>
+          Roadmap: <a href="https://github.com/FormidableLabs/victory/blob/master/ROADMAP.md">
+            ROADMAP.md
+          </a>
+        </p>
+        <p className="Copy">Component docs:</p>
       </section>
     );
   }
