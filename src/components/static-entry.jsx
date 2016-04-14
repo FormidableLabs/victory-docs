455273719406b4d2cce9b0631e6d5a3488a66b28
Paula Lavalle <lavalle.paula@gmail.com>
2016-04-13 17:06:16 -0700

    WIP does our static site plugin even work with rr2.0.0?

--- src/components/static-entry.jsx
+++ src/components/static-entry.jsx
@@ -1,28 +1,57 @@
 import React from "react";
 import { render } from "react-dom";
+import { Router, RouterContext, match, useRouterHistory } from "react-router";
+import { createHistory, createMemoryHistory } from 'history';
+import createBrowserHistory from "history/lib/createBrowserHistory";
+import useScroll from "scroll-behavior/lib/useStandardScroll";
 import { renderToString } from "react-dom/server";
-import { routing } from "./config";
+const routing = { base: "/" };
 
 import App from "./app";
 import Index from "../../templates/index.hbs";
+import routes from "../routes";
+import staticRoutes from "../../static-routes.js";
 
-// Run once we hit the client side
+// ----------------------------------------------------------------------------
+// With `static-site-generator-webpack-plugin`, the same bundle is responsible for
+// both 1.) telling the plugin what to render to HTML and
+// 2.) running the app on the client side. In other words, this entry point
+// the roles of `server/index` and `client/app`.
+// ----------------------------------------------------------------------------
+
+// Client render (optional):
 if (typeof document !== "undefined") {
-  render(<App />, document.getElementById("content"));
+  const appHistory = useScroll(useRouterHistory(createBrowserHistory))();
+  render(
+    <Router
+      history={appHistory}
+      routes={routes}
+    />,
+    document.getElementById("content")
+  );
 }
 
-// Expose the function `static-site-generator-webpack-plugin` needs to build its HTML
-export default (locals, next) => {
-  const source = JSON.parse(locals.webpackStats.compilation.assets["stats.json"].source());
-  // For now, `assets` is a string; once we start bundling CSS too, it'll become an array
-  const assets = source.assetsByChunkName.main;
+// Exported static site renderer:
+export default (locals, callback) => {
+  const history = createMemoryHistory();
+  const location = history.createLocation(locals.path);
 
-  const content = renderToString(<App />);
-  const html = Index({
-    content,
-    bundleJs: assets,
-    baseHref: routing.base
+  console.log("location: ", location);
+
+  match({ routes, location }, (error, redirectLocation, renderProps) => {
+    if (error) {
+      console.log('Error', error);
+    } else if (renderProps) {
+      console.log("render props!!", renderProps);
+      callback(null, Index({
+        content: renderToString(<RouterContext {...renderProps} />),
+        html: renderToString(<RouterContext {...renderProps} />),
+        assets: locals.assets,
+        bundleJs: locals.assets.main,
+        baseHref: routing.base
+      }));
+    } else {
+      console.log('404 Not Found');
+    }
   });
-
-  next(null, html);
 };
