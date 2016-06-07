1b53153a77e49c5eeb37822cd3a4137098b2d16b
Paula Lavalle <lavalle.paula@gmail.com>
2016-06-07 13:01:31 -0700

    lint

--- src/components/static-entry.jsx
+++ src/components/static-entry.jsx
@@ -23,7 +23,7 @@ import routes from "../routes";
 // `static-site-generator-webpack-plugin` supports shimming browser globals
 // so instead of checking whether the document is undefined (always false),
 // Check whether it’s being shimmed
-if (window && window.__STATIC_GENERATOR !== true) {
+if (window && window.__STATIC_GENERATOR !== true) { //eslint-disable-line no-undef
   render(
     <Router
       history={browserHistory}
