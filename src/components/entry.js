a83559552555b764a35a48aeeea3fd70e83e14ae
Brian Mathews <brian.mathews@formidable.com>
2016-07-28 13:35:19 -0700

    Add user-agent for static rendering. Fixes invalid styles

--- src/components/entry.js
+++ src/components/entry.js
@@ -38,6 +38,11 @@ if (typeof window !== "undefined" && window.__STATIC_GENERATOR !== true) { //esl
 
 // Exported static site renderer:
 export default (locals, callback) => {
+  // userAgent for radium vendor prefixing
+  global.navigator = {
+    userAgent: "Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/49.0.2454.85 Safari/537.36"
+  };
+
   const history = createMemoryHistory();
   const location = history.createLocation(locals.path);
 
