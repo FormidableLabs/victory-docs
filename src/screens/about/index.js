57457ccfc9fdd7689c9b610fa6c5f48bd0db0a0e
ebrillhart <emma.brillhart@gmail.com>
2016-09-30 09:41:02 -0700

    fix indentation

--- src/screens/about/index.js
+++ src/screens/about/index.js
@@ -55,8 +55,8 @@ class About extends React.Component {
           <h2 id="showcase">
             Showcase
           </h2>
-            <p style={styles.copy}>See how some companies are using Victory to enhance their
-             services, products, and publications.</p>
+          <p style={styles.copy}>See how some companies are using Victory to enhance their
+            services, products, and publications.</p>
           <Showcase />
 
           <h2 style={styles.copy}>
