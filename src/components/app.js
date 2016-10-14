a529cdf36c38e2dfe77a50fd515601315dadcaed
Paula Lavalle <lavalle.paula@gmail.com>
2016-10-14 12:57:25 -0700

    Add /styles for transitioning to CSS

--- src/components/app.js
+++ src/components/app.js
@@ -4,13 +4,13 @@ import Radium, { Style, StyleRoot } from "radium";
 
 // Variables and Stylesheet
 import { VictoryTheme as VictoryStyleSheet } from "formidable-landers";
+import "../styles/styles.css";
 
 class App extends React.Component {
   render() {
     const isBrowser = typeof window !== "undefined" && window.__STATIC_GENERATOR !== true;
     return (
       <StyleRoot radiumConfig={isBrowser ? { userAgent: window.navigator.userAgent } : null}>
-        <Style rules={VictoryStyleSheet} />
         {this.props.children}
       </StyleRoot>
     );
