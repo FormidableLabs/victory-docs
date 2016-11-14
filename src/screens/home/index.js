f77eee9a55ddae916c212268a4432454e711a449
Paula Lavalle <lavalle.paula@gmail.com>
2016-11-14 11:00:28 -0800

    Add about Formidable to homepage

--- src/screens/home/index.js
+++ src/screens/home/index.js
@@ -26,6 +26,16 @@ class Home extends React.Component {
           <Companies />
 
           <Benefits />
+
+          <div className="u-textCenter u-maxWidthPara u-marginAuto">
+            <h2>We are Formidable</h2>
+            <p>
+              Formidable is a Seattle-based consultancy and open-source shop, with an emphasis on Node.js and React.js. We deploy a mixture of consulting, staff augmentation, and training to level up teams and solve engineering problems. Whether it’s transitioning walmart.com to React, moving speedtest.net off Flash, or helping a startup build and scale an MVP, we’re ready to help teams of any size.
+            </p>
+            <p>
+              Interested in hiring or working for us? <a href="https://formidable.com/contact">Get in touch</a> or view our <a href="https://formidable.com/careers">Careers Page</a>.
+            </p>
+          </div>
         </section>
         <Footer
           style={{
