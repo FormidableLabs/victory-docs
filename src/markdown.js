2db9f2aa6954dcc00ec3c3177ed5317af8274c1e
Ian Walker-Sperber <ianwsperber@gmail.com>
2017-05-04 14:00:21 -0700

    287 Helper for parsing toc from markdown file

--- src/markdown.js
+++ src/markdown.js
@@ -1,7 +1,24 @@
+const _ = require("lodash");
+const marked = require("marked");
 
 // Format a string as an anchor tag ("Foo bar" -> "foo-bar")
 const toAnchor = (content) => content.toLowerCase().replace(/[^\w]+/g, "-");
 
-export default {
-  toAnchor
+const parseToc = (mdContent) => {
+  const toc = [];
+
+  marked(mdContent, {
+    renderer: _.extend(new marked.Renderer(), {
+      heading: (content, level) => {
+        toc.push({ content, level, anchor: toAnchor(content) });
+      }
+    })
+  });
+
+  return toc;
+};
+
+module.exports = {
+  toAnchor,
+  parseToc
 };
