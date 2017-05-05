d0334436b21d2b2d113c2cea933ad63417c32df9
Ian Walker-Sperber <ianwsperber@gmail.com>
2017-05-05 09:57:47 -0700

    287 Reformat subheading content

--- src/components/sidebar/content.js
+++ src/components/sidebar/content.js
@@ -2,28 +2,33 @@ import _ from "lodash";
 import { configDocs } from "../config";
 import { configGuides } from "../config-guides";
 
-const docItems = configDocs.map((item) => _.extend({ type: "item" }, item));
-const guideItems = configGuides.map((item) => _.extend({ type: "item" }, item));
+const docItems = configDocs.map((item) => _.extend({
+  type: "item",
+  route: "guides"
+}, item));
 
-const subHeading = (category, items) => ([
-  {
-    text: category,
-    type: "subheading"
-  },
-  ...items.filter((item) => item.category === category)
-]);
+const guideItems = configGuides.map((item) => _.extend({
+  type: "item",
+  route: "docs"
+}, item));
+
+const subHeading = (items, text) => ({
+  text,
+  type: "subheading",
+  list: items.filter((item) => item.category === text)
+});
 
 export default [
   {
     text: "Guides",
     type: "heading"
   },
-  ...guideItems,
+  subHeading(guideItems),
   {
     text: "Documentation",
     type: "heading"
   },
-  ...subHeading("chart", docItems),
-  ...subHeading("core", docItems),
-  ...subHeading("more", docItems)
+  subHeading(docItems, "chart"),
+  subHeading(docItems, "core"),
+  subHeading(docItems, "more")
 ];
