15f1ca5fb3fa5c37babd54ed8ae757a75c546147
Ian Walker-Sperber <ianwsperber@gmail.com>
2017-05-08 14:21:17 -0700

    287 Remove id

--- src/components/sidebar/content.js
+++ src/components/sidebar/content.js
@@ -2,46 +2,32 @@ import _ from "lodash";
 import { config } from "../config";
 import { configGuides } from "../config-guides";
 
-
-let _counter = -1;
-
-const id = () => {
-  _counter++;
-
-  return _counter;
-};
-
 /* Format the sidebar content as a tree */
 const docItems = config.map((item) => _.extend({
-  id: id(),
   type: "item",
   route: "docs",
-  children: item.toc.map((t) => _.extend({ id: id() }, t))
+  children: item.toc
 }, item));
 
 const guideItems = configGuides.map((item) => _.extend({
-  id: id(),
   type: "item",
   route: "guides",
-  children: item.toc.map((t) => _.extend({ id: id() }, t))
+  children: item.toc
 }, item));
 
 const subHeading = (items, text) => ({
   text,
-  id: id(),
   children: items.filter((item) => item.category === text)
 });
 
 const sidebarContent = [
   {
-    id: id(),
     text: "Guides",
     children: [
       subHeading(guideItems)
     ]
   },
   {
-    id: id(),
     text: "Documentation",
     children: [
       subHeading(docItems, "chart"),
