4c6ecf28835d264a2e1b0cd1afb9ddb94f6ba0c6
Ian Walker-Sperber <ianwsperber@gmail.com>
2017-05-08 12:03:54 -0700

    287 Special formatting of toc when creating search index

--- src/components/sidebar/content.js
+++ src/components/sidebar/content.js
@@ -2,6 +2,51 @@ import _ from "lodash";
 import { config } from "../config";
 import { configGuides } from "../config-guides";
 
+/* Helpers for creating the searchable array */
+const createSearchableArray = (node, ancestors) => {
+  ancestors = ancestors || "";
+  const searchPart = node.text || node.content || "";
+  const searchText = `${ancestors} ${searchPart}`.trim();
+
+  let nodes = [{ searchText, searchPart }];
+
+  if (!node.children || !node.children.length) {
+    return nodes;
+  }
+
+  let children = [];
+
+  if (node.type === "item") {
+    const levels = [];
+
+    children = node.children
+      .filter((child) => child.level > 1)
+      .map((child) => {
+        const childSearchPart = child.content;
+
+        levels[child.level - 2] = childSearchPart;
+        levels.fill(null, child.level - 1);
+
+        const childAncestors = levels.slice(0, child.level).join(" ").trim();
+        const childSearchText = `${searchText} ${childAncestors}`;
+
+        return {
+          searchPart: childSearchPart,
+          searchText: childSearchText
+        };
+      });
+  } else {
+    children = node.children.reduce((prev, child) => {
+      return prev.concat(createSearchableArray(child, searchText));
+    }, []);
+  }
+
+  nodes = nodes.concat(children);
+
+  return nodes;
+};
+
+/* Format the sidebar content as a tree */
 const docItems = config.map((item) => _.extend({
   type: "item",
   route: "docs",
@@ -18,24 +63,6 @@ const subHeading = (items, text) => ({
   text,
   children: items.filter((item) => item.category === text)
 });
-
-const getNodesWithAncestors = (node, ancestors) => {
-  ancestors = ancestors || "";
-  const searchPart = node.text || node.content || "";
-  const searchText = `${ancestors} ${searchPart}`.trim();
-
-  let nodes = [{ searchText, searchPart }];
-
-  nodes.push();
-
-  if (node.children) {
-    nodes = nodes.concat(node.children.reduce((prev, child) => {
-      return prev.concat(getNodesWithAncestors(child, searchText));
-    }, []));
-  }
-
-  return nodes;
-};
 
 const sidebarContent = [
   {
@@ -54,9 +81,12 @@ const sidebarContent = [
   }
 ];
 
+/* Create the searchable array */
+const searchIndex = sidebarContent.reduce((prev, current) => {
+  return prev.concat(createSearchableArray(current));
+}, []);
+
 module.exports = {
   sidebarContent,
-  searchIndex: sidebarContent.reduce((prev, current) => {
-    return prev.concat(getNodesWithAncestors(current));
-  }, [])
+  searchIndex
 };
