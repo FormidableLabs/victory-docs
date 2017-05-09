0e1951518c8a2d2f6c619755569506c8a4a7fbb5
Ian Walker-Sperber <ianwsperber@gmail.com>
2017-05-09 16:19:03 -0700

    287 Store additional info in index

--- src/components/sidebar/search.js
+++ src/components/sidebar/search.js
@@ -1,49 +1,67 @@
 import _ from "lodash";
 
-/* Helpers for creating the searchable array */
-const createSearchIndex = (node, parents) => {
-  const getText = (n) => n.text || n.content || "";
+const getText = (node) => node.text || node.content || "";
 
+const createSearchIndexForItem = (itemNode, parentText, parentIds) => {
+  // h1's are assumed to be duplicates of their parent
+  const minHeadingLevel = 2;
+
+  // Contains state!
+  const levels = [];
+
+  return itemNode.children
+    .filter((child) => child.level >= minHeadingLevel)
+    .map((child) => {
+      levels[child.level - minHeadingLevel] = child;
+      levels.fill(null, child.level - (minHeadingLevel - 1));
+      const relevantNodes = levels.slice(0, child.level).filter((n) => n);
+
+      const childParentText = parentText;
+      const childSearchText = relevantNodes
+        .map((n) => getText(n))
+        .join(" ")
+        .trim();
+      const childNodeText = getText(child);
+      const childFullText = `${childParentText} ${childSearchText}`;
+      const childIds = parentIds.concat(relevantNodes.map((n) => n.id));
+
+      return {
+        ids: childIds,
+        parentText: childParentText,
+        fullText: childFullText,
+        nodeText: childNodeText,
+        isLeaf: true
+      };
+    });
+};
+
+/* Helpers for creating the searchable array */
+const createSearchIndex = (node, parents = []) => {
   if (_.isArray(node)) {
     return node.reduce((prev, current) => {
       return prev.concat(createSearchIndex(current, parents));
     }, []);
   }
 
-  const currentLineage = (parents || []).concat([node]);
-  const searchText = currentLineage.map((n) => getText(n)).join(" ").trim();
+  const currentLineage = parents.concat([node]);
+  const fullText = currentLineage.map((n) => getText(n)).join(" ").trim();
+  const parentText = parents.map((n) => getText(n)).join(" ").trim();
+  const nodeText = getText(node);
   const ids = currentLineage.map((n) => n.id);
 
-  const nodes = [{ searchText, ids }];
+  const nodes = [{
+    parentText,
+    fullText,
+    nodeText,
+    ids,
+    isLeaf: !(node.children && node.children.length)
+  }];
   let children = [];
 
   if (node.type === "item") {
     // Special logic for indexing toc (eg "props"),
     // which is stored as an array, not a tree.
-
-    // h1's are assumed to be duplicates of their parent
-    const minHeadingLevel = 2;
-    const levels = [];
-
-    children = node.children
-      .filter((child) => child.level >= minHeadingLevel)
-      .map((child) => {
-        levels[child.level - minHeadingLevel] = child;
-        levels.fill(null, child.level - (minHeadingLevel - 1));
-        const relevantNodes = levels.slice(0, child.level).filter((n) => n);
-
-        const childText = relevantNodes
-          .map((n) => getText(n))
-          .join(" ")
-          .trim();
-        const childSearchText = `${searchText} ${childText}`;
-        const childIds = ids.concat(relevantNodes.map((n) => n.id));
-
-        return {
-          ids: childIds,
-          searchText: childSearchText
-        };
-      });
+    children = createSearchIndexForItem(node, fullText, ids);
   } else if (node.children) {
     children = createSearchIndex(node.children, currentLineage);
   }
@@ -56,7 +74,7 @@ const getMatching = (text, arr) => {
 
   return arr
     .filter((n) => {
-      return n.searchText.toLowerCase().includes(term);
+      return n.fullText.toLowerCase().includes(term);
     })
     .reduce((prev, n) => {
       return n.ids.reduce((ids, id) => _.extend(ids, { [id]: true }), prev);
