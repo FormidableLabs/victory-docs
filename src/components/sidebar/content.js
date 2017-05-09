c06af26500162cc16cf600760b8c5cc989cc8cca
Ian Walker-Sperber <ianwsperber@gmail.com>
2017-05-09 10:52:22 -0700

    287 Add id to sidebar content

--- src/components/sidebar/content.js
+++ src/components/sidebar/content.js
@@ -3,12 +3,21 @@ import { config } from "../config";
 import { configGuides } from "../config-guides";
 import marked from "marked";
 
+let _counter = -1;
+
+const id = () => {
+  _counter++;
+
+  return _counter;
+};
+
 const formatToc = (toc) => {
   const inlineRenderer = _.extend(new marked.Renderer(), {
     paragraph: _.identity
   });
 
   return toc.map((t) => _.extend({
+    id: id(),
     markdown: marked(t.content, {
       gfm: true,
       renderer: inlineRenderer
@@ -18,12 +27,14 @@ const formatToc = (toc) => {
 
 /* Format the sidebar content as a tree */
 const docItems = config.map((item) => _.extend({
+  id: id(),
   type: "item",
   route: "docs",
   children: formatToc(item.toc)
 }, _.omit(item, "toc")));
 
 const guideItems = configGuides.map((item) => _.extend({
+  id: id(),
   type: "item",
   route: "guides",
   children: formatToc(item.toc)
@@ -31,27 +42,31 @@ const guideItems = configGuides.map((item) => _.extend({
 
 const subHeading = (items, text) => ({
   text,
+  id: id(),
   children: items.filter((item) => item.category === text)
 });
 
 const sidebarContent = [
   {
+    id: id(),
     "text": "Introduction",
     // Introduction HTML is manually constructed
     "render": false,
     children: [
-      { text: "Getting Started" },
-      { text: "Native" },
-      { text: "Contributing" }
+      { id: id(), text: "Getting Started" },
+      { id: id(), text: "Native" },
+      { id: id(), text: "Contributing" }
     ]
   },
   {
+    id: id(),
     text: "Guides",
     children: [
       subHeading(guideItems)
     ]
   },
   {
+    id: id(),
     text: "Documentation",
     children: [
       subHeading(docItems, "chart"),
