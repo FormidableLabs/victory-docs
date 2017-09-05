1688d0b5f333c8b96b8732fca77c72326e34a129
Paula Lavalle <paulathevalley@users.noreply.github.com>
2017-09-05 16:15:41 -0700

    Add redfin tune and zillow logos to homepage (#419)

--- src/screens/home/components/companies.js
+++ src/screens/home/components/companies.js
@@ -9,6 +9,9 @@ import VIACOM from "../../../../static/logo-viacom.svg";
 import FIVETHIRTYEIGHT from "../../../../static/logo-fivethirtyeight.svg";
 import USAFACTS from "../../../../static/logo-usafacts.svg";
 import AIRBNB from "../../../../static/logo-airbnb.svg";
+import REDFIN from "../../../../static/logo-redfin.svg";
+import TUNE from "../../../../static/logo-tune.svg";
+import ZILLOW from "../../../../static/logo-zillow.svg";
 
 class Companies extends React.Component {
   render() {
@@ -19,9 +22,14 @@ class Companies extends React.Component {
         </h2>
         <ul className="Companies-List">
           <li className="Companies-logo" style={{maxWidth: "171px"}} dangerouslySetInnerHTML={{__html: AIRBNB}} />
-          <li className="Companies-logo" style={{maxWidth: "350px"}} dangerouslySetInnerHTML={{__html: USAFACTS}} />
           <li className="Companies-logo" style={{maxWidth: "300px"}} dangerouslySetInnerHTML={{__html: FIVETHIRTYEIGHT}} />
+          <li className="Companies-logo" style={{maxWidth: "350px"}} dangerouslySetInnerHTML={{__html: USAFACTS}} />
+        </ul>
+        <ul className="Companies-List">
           <li className="Companies-logo" style={{maxWidth: "200px"}} dangerouslySetInnerHTML={{__html: VIACOM}} />
+          <li className="Companies-logo" style={{maxWidth: "177px", paddingTop: "2px"}} dangerouslySetInnerHTML={{__html: REDFIN}} />
+          <li className="Companies-logo" style={{maxWidth: "190px", paddingBottom: "3px"}} dangerouslySetInnerHTML={{__html: ZILLOW}} />
+          <li className="Companies-logo" style={{maxWidth: "141px"}} dangerouslySetInnerHTML={{__html: TUNE}} />
         </ul>
         <p className="SubHeading">
           <Link to="/about#showcase" className="isLight">
