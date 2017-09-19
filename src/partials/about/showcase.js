389891587d11fe1c9c528ddbd5e5ca9e70d21980
Paula Lavalle <lavalle.paula@gmail.com>
2017-09-19 12:02:18 -0700

    Finalize about page

--- src/partials/about/showcase.js
+++ src/partials/about/showcase.js
@@ -1,5 +1,10 @@
 import React from "react";
 import ShowcaseApp from "./showcase-app";
+// Images
+import IMG538 from "../../../static/showcase-fivethirtyeight.png";
+import IMGPOSTMARK from "../../../static/showcase-postmark.png";
+import IMGTUNE from "../../../static/showcase-tune.png";
+import IMGCUMUL8 from "../../../static/showcase-cumul8.png";
 
 class Showcase extends React.Component {
   getShowcaseApps() {
@@ -7,7 +12,7 @@ class Showcase extends React.Component {
       {
         company: "FiveThirtyEight",
         screenshot: {
-          src: "/static/showcase-fivethirtyeight.png",
+          src: IMG538,
           alt: "FiveThirtyEight screenshot"
         },
         description: "FiveThirtyEight uses Victory for data visualizations, including this one about the history of sumo wrestling."
@@ -15,7 +20,7 @@ class Showcase extends React.Component {
       {
         company: "Postmark",
         screenshot: {
-          src: "/static/showcase-postmark.png",
+          src: IMGPOSTMARK,
           alt: "Postmark screenshot"
         },
         description: "Postmark uses Victory on its status page to let users know how their service is performing."
@@ -23,7 +28,7 @@ class Showcase extends React.Component {
       {
         company: "Tune",
         screenshot: {
-          src: "/static/showcase-tune.png",
+          src: IMGTUNE,
           alt: "Tune screenshot"
         },
         description: "Tune uses Victory in its Marketing Console, a tool for marketers to track all of their mobile data."
@@ -31,7 +36,7 @@ class Showcase extends React.Component {
       {
         company: "Cumul8",
         screenshot: {
-          src: "/static/showcase-cumul8.png",
+          src: IMGCUMUL8,
           alt: "Cumul8 screenshot"
         },
         description: "Cumul8 uses Victory for Cirrus, a SaaS platform for businesses to manage their internal data."
