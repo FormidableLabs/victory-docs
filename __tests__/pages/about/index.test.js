2bb171c63aaca5f2b12d541b1b710aa6e57b85ab
boygirl <lauren.eastridge@formidable.com>
2018-06-08 12:15:03 -0700

    update test infrastructure

--- __tests__/pages/about/index.test.js
+++ __tests__/pages/about/index.test.js
@@ -1,7 +1,10 @@
 /* global describe, test, expect */
 import React from "react";
-import { mount } from "enzyme";
+import Enzyme, { mount } from "enzyme";
+import Adapter from "enzyme-adapter-react-16";
 import About from "../../../src/pages/about/index";
+
+Enzyme.configure({ adapter: new Adapter() });
 
 describe("<About />", () => {
   test("should render correctly.", () => {
