65645fa90352a255151fcb07f4b820bcf5ae4e61
boygirl <lauren.eastridge@formidable.com>
2019-03-15 14:02:23 -0700

    prettier

--- test/formidable-nav-integration.js
+++ test/formidable-nav-integration.js
@@ -3,22 +3,36 @@ const path = require("path");
 
 try {
   (async () => {
-    const browser = await puppeteer.launch({args: ['--no-sandbox']});
+    const browser = await puppeteer.launch({ args: ["--no-sandbox"] });
     const page = await browser.newPage();
     await page.setViewport({ width: 1280, height: 800 });
-    await page.goto('http://localhost:3000/open-source/victory/');
-    const links = ["about", "work", "careers", "open-source", "blog", "contact"];
+    await page.goto("http://localhost:3000/open-source/victory/");
+    const links = [
+      "about",
+      "work",
+      "careers",
+      "open-source",
+      "blog",
+      "contact"
+    ];
     for (let i = 0; i < links.length; i++) {
       const l = links[i];
       await page.click(`[href='https://formidable.com/${l}']`);
       await page.waitFor(2500);
-      await page.screenshot({path:  path.basename(process.cwd()) === "test" ?
-          `screenshots/${l}.png`: `test/screenshots/${l}-formidable.png`, type: "png"});
+      await page.screenshot({
+        path:
+          path.basename(process.cwd()) === "test"
+            ? `screenshots/${l}.png`
+            : `test/screenshots/${l}-formidable.png`,
+        type: "png"
+      });
       await page.goBack();
-      console.log(`See resolved route screenshot at screenshots/${l}-formidable`);
+      console.log(
+        `See resolved route screenshot at screenshots/${l}-formidable`
+      );
     }
-    await browser.close()
-  })()
+    await browser.close();
+  })();
 } catch (err) {
   throw err;
 }
--- test/victory-nav-integration.js
+++ test/victory-nav-integration.js
@@ -3,10 +3,10 @@ const path = require("path");
 
 try {
   (async () => {
-    const browser = await puppeteer.launch({args: ['--no-sandbox']});
+    const browser = await puppeteer.launch({ args: ["--no-sandbox"] });
     const page = await browser.newPage();
     await page.setViewport({ width: 1280, height: 800 });
-    await page.goto('http://localhost:3000/open-source/victory/');
+    await page.goto("http://localhost:3000/open-source/victory/");
     const links = ["about", "docs", "docs/faq", "guides", "gallery"];
     const markdownEndpoints = ["docs", "docs/faq", "guides"];
     for (let i = 0; i < links.length; i++) {
@@ -16,11 +16,18 @@ try {
       if (markdownEndpoints.includes(l)) {
         const heading = await page.waitForSelector(".Recipe h1");
         if (!heading) {
-          throw new Error(`Expected Markdown did not render as expected for route ${l}!`)
+          throw new Error(
+            `Expected Markdown did not render as expected for route ${l}!`
+          );
         }
       }
-      await page.screenshot({path:  path.basename(process.cwd()) === "test" ?
-          `screenshots/${l}.png`: `test/screenshots/${l}-victory.png`, type: "png"});
+      await page.screenshot({
+        path:
+          path.basename(process.cwd()) === "test"
+            ? `screenshots/${l}.png`
+            : `test/screenshots/${l}-victory.png`,
+        type: "png"
+      });
       console.log(`See resolved route screenshot at screenshots/${l}-victory`);
     }
     // we can do these too, but really validating that they exist is probably sufficient, lemme know!
@@ -31,8 +38,8 @@ try {
     //   await page.waitFor(2000);
     //   await page.goBack();
     // }
-    await browser.close()
-  })()
+    await browser.close();
+  })();
 } catch (err) {
   throw err;
 }
