354978015e94b922c1054ef345b5a5ad209654b0
Brittany Feenstra <brittanyfeenstra@Brittanys-MacBook-Pro-2.local>
2020-01-21 14:34:35 -0700

    working scroll that's also ugly w/ backup

--- src/app.js
+++ src/app.js
@@ -1,10 +1,9 @@
-import React, { useLayoutEffect, useState } from "react";
+import React, { useLayoutEffect, useEffect } from "react";
 import PropTypes from "prop-types";
 import { Root, Routes } from "react-static";
 import { Route } from "react-router";
 import { ThemeProvider } from "styled-components";
 import { animateScroll as scroll } from "react-scroll";
-import { isEmpty } from "lodash/isEmpty";
 
 import GlobalStyle from "./styles/global";
 import theme from "./styles/theme";
@@ -42,14 +41,55 @@ const scrollContent = async (
 const checkScrollRoutes = (pathname, routes = ROUTES) =>
   routes.some(r => pathname.includes(r));
 
+// Back up implementation without hooks to show the problem with getting the scroll of hashes on docs to work
+// this class component isn't actually in use - you can swap it out with `ScrollToCurrentSection` and get the same effect.
+// I am truly stumped on how to get this working better and I know this is very ugly but it's also helpful to demonstrate where I'm stuck.
+
+class ScrollToTop extends React.Component {
+  componentDidMount() {
+    window.scrollTo(0, 0);
+    if (this.props.location && this.props.location.hash) {
+      setTimeout(() => {
+        scrollContent(this.props.location.hash);
+      }, 1000);
+    }
+  }
+
+  componentDidUpdate() {
+    if (checkScrollRoutes(this.props.location.pathname)) {
+      scrollContent(this.props.location.hash);
+    }
+  }
+
+  render() {
+    return this.props.children;
+  }
+}
+ScrollToTop.propTypes = {
+  children: PropTypes.array,
+  location: PropTypes.object
+};
+
 const ScrollToCurrentSection = ({ location, children }) => {
   const { pathname, hash = "" } = location;
+  useEffect(() => {
+    window.scrollTo(0, 0);
+    if (checkScrollRoutes(pathname) && hash) {
+      setTimeout(() => {
+        scrollContent(hash);
+      }, 1000);
+    }
+  }, [pathname]);
+  // So, even though we're using the hash here for scroll content we only want this useEffect to fire when the pathname changes
+
   useLayoutEffect(() => {
     if (checkScrollRoutes(pathname)) {
       scrollContent(hash);
     }
   }, [hash, pathname]);
-
+  // this will handle route scrolls after initial paint. The location of the hash isn't able to be identified on the initial paint in time for the scroll to work reliably.
+  // it just needs the _slightest_ delay for it to work. Slow 3G on chrome still works.
+  // Please, please, please show me a better way to navigate this.
   return children;
 };
 
