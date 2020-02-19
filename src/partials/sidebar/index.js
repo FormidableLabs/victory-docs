f78eea57d2988a9ceb8306b64fa8256d01388d39
Brittany Feenstra <brittanyfeenstra@Brittanys-MacBook-Pro-2.local>
2020-02-19 16:03:00 -0700

    unnecessary hook

--- src/partials/sidebar/index.js
+++ src/partials/sidebar/index.js
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from "react";
+import React, { useState } from "react";
 import PropTypes from "prop-types";
 import { withRouteData } from "react-static";
 import styled from "styled-components";
